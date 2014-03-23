28beb39152524f456d378afbcf85ea8f1ed49821
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-03-23 23:42:18 +0100

    Rule #2
    Any live cell with two or three live neighbours lives on to the next generation.

--- lib/gol/world.rb
+++ lib/gol/world.rb
@@ -1,14 +1,52 @@
 class World
 
-  def create_life_at(x, y)
+  def initialize
+    @alife_cells = Set.new
+  end
 
+  def create_life_at(x, y)
+    @alife_cells.add(Point.new(x, y))
   end
 
   def tick!
+    new_generation = @alife_cells.select do |point|
+      neighbours_count(point).between?(2, 3)
+    end
 
+    @alife_cells = new_generation.to_set
   end
 
   def has_life_at?(x, y)
+    @alife_cells.include?(Point.new(x, y))
+  end
 
+  private
+
+  @@neighbours_offset = [
+    [-1, -1], [-1, 0], [-1, 1],
+    [0, -1], [0, 1],
+    [1, -1], [1, 0], [1, 1]
+  ]
+
+  def neighbours_count(point)
+    @@neighbours_offset.select do |offset|
+      has_life_at?(point.x + offset[0], point.y + offset[1])
+    end.size
+  end
+
+  class Point
+    attr_reader :x, :y
+
+    def initialize(x, y)
+      @x, @y = x, y
+    end
+
+    def eql?(point)
+      x == point.x && y == point.y
+    end
+
+    def hash
+      [x, y].hash
+    end
   end
 end
--- spec/lib/gol/world_spec.rb
+++ spec/lib/gol/world_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe World do
 
-  context 'Rule #1: Any live cell with fewer than two live neighbours dies, as if caused by under-population.' do
+  before do
+    subject.create_life_at(1, 1)
+  end
 
-    before do
-      subject.create_life_at(1, 1)
-    end
+  context 'Rule #1: Any live cell with fewer than two live neighbours dies, as if caused by under-population.' do
 
     it 'cell is dead when no neighbours' do
       subject.tick!
@@ -20,6 +20,27 @@ describe World do
       subject.tick!
 
       expect(subject.has_life_at?(1, 1)).to be_false
+    end
+  end
+
+  context 'Rule #2: Any live cell with two or three live neighbours lives on to the next generation.' do
+    it 'cell stays alife when has 2 neighbour' do
+      subject.create_life_at(1, 0)
+      subject.create_life_at(1, 2)
+
+      subject.tick!
+
+      expect(subject.has_life_at?(1, 1)).to be_true
+    end
+
+    it 'cell stays alife when has 3 neighbour' do
+      subject.create_life_at(1, 0)
+      subject.create_life_at(1, 2)
+      subject.create_life_at(0, 0)
+
+      subject.tick!
+
+      expect(subject.has_life_at?(1, 1)).to be_true
     end
   end
 end
